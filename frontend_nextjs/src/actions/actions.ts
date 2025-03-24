ee465ebf8d7dbb67740fe6a98216250cd3ddad1a
galihadityam1 <galihadityam1@gmail.com>
2025-03-24 22:54:11 +0700

    add button delete task

--- frontend_nextjs/src/actions/actions.ts
+++ frontend_nextjs/src/actions/actions.ts
@@ -1,3 +1,4 @@
+import { Employee } from "@/Types";
 import axios from "axios";
 import Cookies from "universal-cookie";
 
@@ -15,7 +16,7 @@ export const storeTask = async (task: Task, refreshData: () => void) => {
     try {
         const cookies = new Cookies()
         const xsrfToken = cookies.get("XSRF-TOKEN");
-        await axios.post(
+            await axios.post(
             "http://127.0.0.1:8000/api/task",
             { ...task },
             {
@@ -32,3 +33,25 @@ export const storeTask = async (task: Task, refreshData: () => void) => {
         console.error("Error submitting task:", error);
     }
 };
+
+export const deleteTask = async (id: Employee["id"]) => {
+    try {
+        const cookies = new Cookies();
+        const xsrfToken = cookies.get("XSRF-TOKEN");
+        let response = await axios.delete(
+            `http://127.0.0.1:8000/api/taskdelete/${id}`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": xsrfToken,
+                },
+                withCredentials: true
+            }
+        )
+
+        return response        
+    } catch (error) {
+        console.log("Error", error);
+        
+    }
+}
--- frontend_nextjs/src/app/page.tsx
+++ frontend_nextjs/src/app/page.tsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { Employee, Task } from "@/Types";
 import AddTask from "@/components/AddTask";
-import { fetchEmployee } from "@/actions/actions";
+import { deleteTask, fetchEmployee } from "@/actions/actions";
+import Swal from "sweetalert2";
 
 export default function Home() {
   const [employees, setEmployees] = useState([]);
@@ -13,9 +14,28 @@ export default function Home() {
       .catch(error => console.error(error));
   }
 
+  async function deleteData(id: number) {
+    const res = await deleteTask(id)
+
+    if (res?.status !== 200) {
+      return Swal.fire({
+        title: 'Fail to delete task',
+        showConfirmButton: false,
+        timer: 1500,
+        icon: 'warning'
+      })
+    }
+
+    return Swal.fire({
+      title: "Success!",
+      text: "Task has deleted!",
+      icon: "success"
+    })
+  }
+
   useEffect(() => {
     fetchData()
-  }, []);
+  }, [deleteData]);
 
   return (
     <div className="container mt-5">
@@ -26,6 +46,7 @@ export default function Home() {
             <th>Nama Karyawan</th>
             <th>Task</th>
             <th>Due Date</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -36,12 +57,18 @@ export default function Home() {
                   <td>{emp.name}</td>
                   <td>{task.task_name}</td>
                   <td>{new Date(task.due_date).toLocaleDateString()}</td>
+                  <td>
+                    <button onClick={() => deleteData(task.id)}>
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))
             ) : (
               <tr key={`no-task-${emp.id}`}>
                 <td>{emp.name}</td>
                 <td colSpan={2} style={{ textAlign: "center" }}>No tasks assigned</td>
+                <td></td>
               </tr>
             )
           ))}
